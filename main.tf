5bc95f93f53fcdbb4f82d6054c91bd2f6ef78e6e
ignaskar <ignas.karpusenkovas@gmail.com>
2021-02-08 21:43:51 +0200

    fix main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ provider "azurerm" {
 terraform {
   backend "azurerm" {
     resource_group_name   = "tf_rg_blobstore"
-    storage_account_name  = "tfstorageaccountignaskar"
+    storage_account_name  = "tfstorageignaskar"
     container_name        = "tfstate"
     key                   = "terraform.tfstate"
   }
